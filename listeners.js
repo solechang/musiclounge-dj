adc85856e1acca81683a247476f16211c419584c
Andrew Kwon <askwon@ucdavis.edu>
2015-12-22 04:22:39 -0800

    Added and implemented sendHostInfo, processHostInfo, and joinLounge listeners

--- listeners.js
+++ listeners.js
@@ -1,12 +1,17 @@
 // Websocket action handlers
+//
+// Add the user and song information in the Lounge JSON object
 function hostLounge(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
     console.log(`--> in hostLounge: ${JSON.stringify(data, null, 2)}`);
     global.Lounges[data.userId] = data;
     global.Lounges[data.userId].socket = ws;
     ws.userId = data.userId;
 }
 
+// Remove the host's name from the Lounge JSON object
 function unhostLounge(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
     console.log(`--> in unhostLounge: ${JSON.stringify(data, null, 2)}`);
     var message = {
         action: "kickJoiner",
@@ -16,12 +21,21 @@ function unhostLounge(ws, data) {
     delete global.Lounges[data.userId];
 }
 
-function updateJoiner(ws, data) {
-    console.log(`--> in updateJoiner: ${JSON.stringify(data, null, 2)}`);
+// Add the joiner name to the list of clients in their desired host's current 
+// lounge and request for the lounge information
+function joinLounge(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+    console.log(`--> in joinLounge: ${JSON.stringify(data, null, 2)}`);
+    if(!global.Lounges[data.hostId].clientIds)
+        global.Lounges[data.hostId].clientIds = [];
+    global.Lounges[data.hostId].clientIds.push(data.userId);
+    requestHostInfo(ws, data);
 }
 
-function sendHostInfo(ws, data) {
-    console.log(`--> in sendHostInfo: ${JSON.stringify(data, null, 2)}`);
+// Request information from the host necessary to play the lounge's current song
+function requestHostInfo(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+    console.log(`--> in requestHostInfo: ${JSON.stringify(data, null, 2)}`);
     var message = {
         action: "requestHostInfo",
         data: { }
@@ -29,16 +43,27 @@ function sendHostInfo(ws, data) {
     ws.send(message);
 }
 
-// Add the joiner name to the list of clients in their desired host's current 
-// lounge and request for the lounge information
-function joinLounge(ws, data) {
-    console.log(`--> in joinLounge: ${JSON.stringify(data, null, 2)}`);
-    if(!global.Lounges[data.hostId].clientIds)
-        global.Lounges[data.hostId].clientIds = [];
-    global.Lounges[data.hostId].clientIds.push(data.userId);
+// The host will send the lounge and current song information to the joiner
+function sendHostInfo(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+    console.log(`--> in sendHostInfo: ${JSON.stringify(data, null, 2)}`);
+    var json = {
+        action: "sendHostInfo",
+        data: data
+    }
+    processHostInfo(ws, json);
 }
 
+// Process the host lounge information to be able to start the song in sync on the joiner's end
+function processHostInfo(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+    console.log(`--> in processHostInfo: ${JSON.stringify(data, null, 2)}`);
+    ws.send(data);
+}
+
+// Remove the joiner name from the list of clients in the joined host's current lounge
 function leaveLounge(ws, data) {
+    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
     console.log(`--> in leaveLounge: ${JSON.stringify(data, null, 2)}`);
 }
 
