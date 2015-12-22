cfd0633546f834884e00c40589854dafa6fe2415
Andrew Kwon <askwon@ucdavis.edu>
2015-12-22 03:18:46 -0800

    Updated the listeners to reflect the most recent protocol

--- listeners.js
+++ listeners.js
@@ -1,27 +1,47 @@
-function broadcast(ids, data) {
-    ids.forEach(id => global.Lounges[id].socket.send(data));
-}
-
+// Websocket action handlers
 function hostLounge(ws, data) {
     console.log(`--> in hostLounge: ${JSON.stringify(data, null, 2)}`);
     global.Lounges[data.userId] = data;
     global.Lounges[data.userId].socket = ws;
+    ws.userId = data.userId;
+}
 
-    broadcast([data.userId], 'whoa');
+function unhostLounge(ws, data) {
+    console.log(`--> in unhostLounge: ${JSON.stringify(data, null, 2)}`);
+}
+
+function updateJoiner(ws, data) {
+    console.log(`--> in updateJoiner: ${JSON.stringify(data, null, 2)}`);
 }
 
 function joinLounge(ws, data) {
     console.log(`--> in joinLounge: ${JSON.stringify(data, null, 2)}`);
 }
 
-function error(ws, data) {
-    console.log(`--> in error: ${JSON.stringify(data, null, 2)}`);
+function leaveLounge(ws, data) {
+    console.log(`--> in leaveLounge: ${JSON.stringify(data, null, 2)}`);
 }
 
+// Auxiliary functions
+function broadcast(ids, data) {
+    ids.forEach(id => global.Lounges[id].socket.send(data));
+}
+
+function success() {
+    
+}
+
+function error() {
+
+}
+
+// Constant object for export
 const actions = {
     hostLounge, 
+    unhostLounge,
+    updateJoiner,
     joinLounge, 
-    error
+    leaveLounge,
 }
 
 module.exports = actions
