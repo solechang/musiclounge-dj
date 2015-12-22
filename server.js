9a27a50fe88ed208f44ba343242cd3a3fbdffdec
Andrew Kwon <askwon@ucdavis.edu>
2015-12-21 23:37:57 -0800

    Incorporated the exported listener functions into the websocket message handler

--- server.js
+++ server.js
@@ -3,24 +3,37 @@ var WebsocketServer = require('ws').Server;
 var ws = new WebsocketServer({ server : server });
 var express = require('express');
 var app = express();
+var listeners = require('./listeners.js');
 
 // Simply send a message to the connected client
 app.use((req, res) => {
     res.send('hi');
 });
 
+// Declare the action handlers
+const actions = {
+    'hostLounge'    : listeners.hostLounge,
+    'joinLounge'    : listeners.joinLounge,
+    'error'         : listeners.error 
+};
+
 // Instantiate listeners upon a websocket connection 
 ws.on('connection', ws => {
     console.log('Client connected to server!');
 
-    ws.on('message', json => {
-        console.log(`Received message from the client: ${json}`);
+    // Listener to handle incoming client messages
+    ws.on('message', data => {
+        console.log(`Received: ${JSON.stringify(data, null, 2)}`);
+        var json = JSON.parse(message.data);
+        actions[json.action](json.data);
     });
 
+    // Listener to handle a client disconnection
     ws.on('close', () => {
         console.log('Client disconnected from the server!');
     });
 });
+
 server.on('request', app);
 server.listen(1337, () => {
     console.log('🌏  Server listening on port 1337');
