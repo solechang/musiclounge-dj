7ed13c2a9231015df3671569f7acc9dcdb22a2f5
Andrew Kwon <askwon@ucdavis.edu>
2015-12-22 02:42:58 -0800

    Implemented a socket broadcast utility function and storage of websockets in the host lounge action handler

--- listeners.js
+++ listeners.js
@@ -1,12 +1,20 @@
-function hostLounge(data) {
-    console.log(`--> in hostLounge: ${JSON.stringify(data, null, 2)}`);
+function broadcast(ids, data) {
+    ids.forEach(id => global.Lounges[id].socket.send(data));
 }
 
-function joinLounge(data) {
+function hostLounge(ws, data) {
+    console.log(`--> in hostLounge: ${JSON.stringify(data, null, 2)}`);
+    global.Lounges[data.userId] = data;
+    global.Lounges[data.userId].socket = ws;
+
+    broadcast([data.userId], 'whoa');
+}
+
+function joinLounge(ws, data) {
     console.log(`--> in joinLounge: ${JSON.stringify(data, null, 2)}`);
 }
 
-function error(data) {
+function error(ws, data) {
     console.log(`--> in error: ${JSON.stringify(data, null, 2)}`);
 }
 
